bc94f63ce460fa9f6c45dd9be07dcb7d29b16e30
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-07-21 07:55:02 +0900

    [fix] User -> Optional<User>로 변경

--- src/main/java/kusitms/backend/domain/user/repository/UserRepository.java
+++ src/main/java/kusitms/backend/domain/user/repository/UserRepository.java
@@ -3,6 +3,8 @@ package kusitms.backend.domain.user.repository;
 import kusitms.backend.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByProviderAndProviderId(String provider, String providerId);
+    Optional<User> findByProviderAndProviderId(String provider, String providerId);
 }
