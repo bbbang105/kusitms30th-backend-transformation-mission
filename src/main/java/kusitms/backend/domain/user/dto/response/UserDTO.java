466416271a7869fa1daac26f2ef5bae3f7f12eb7
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-07-21 03:07:07 +0900

    [feat] UserDTO 생성

---
+++ src/main/java/kusitms/backend/domain/user/dto/response/UserDTO.java
@@ -0,0 +1,13 @@
+package kusitms.backend.domain.user.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+
+    private String role;
+    private String name;
+    private String username;
+}
--- src/main/java/kusitms/backend/domain/user/service/CustomOAuth2UserService.java
+++ src/main/java/kusitms/backend/domain/user/service/CustomOAuth2UserService.java
@@ -3,6 +3,7 @@ package kusitms.backend.domain.user.service;
 import kusitms.backend.domain.user.dto.response.GoogleResponse;
 import kusitms.backend.domain.user.dto.response.NaverResponse;
 import kusitms.backend.domain.user.dto.response.OAuth2Response;
+import kusitms.backend.domain.user.dto.response.UserDTO;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -31,7 +32,15 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
             return null;
         }
 
+        //리소스 서버에서 받은 유저 정보로 사용자를 특정할 providerId (플랫폼 제공 고유 ID)가 있어야 함
+        String username = oAuth2Response.getProvider()+" "+oAuth2Response.getProviderId();
 
+        UserDTO userDTO = new UserDTO();
+        userDTO.setUsername(username);
+        userDTO.setName(oAuth2Response.getName());
+        userDTO.setRole("ROLE_USER");
+
+        return new CustomOAuth2User(userDTO);
 
     }
 }
