58e41bb67f322ee11808dc39a5700fa8157e32b1
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-07-21 05:34:48 +0900

    [feat] localhost:5173 CORS 설정

--- src/main/java/kusitms/backend/global/config/SecurityConfig.java
+++ src/main/java/kusitms/backend/global/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package kusitms.backend.global.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import kusitms.backend.domain.oauth2.CustomSuccessHandler;
 import kusitms.backend.domain.user.service.CustomOAuth2UserService;
 import kusitms.backend.global.jwt.JWTFilter;
@@ -12,6 +13,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -30,6 +35,28 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                        CorsConfiguration configuration = new CorsConfiguration();
+
+                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
+                        configuration.setAllowedMethods(Collections.singletonList("*"));
+                        configuration.setAllowCredentials(true);
+                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+                        configuration.setMaxAge(3600L);
+
+                        configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
+                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                        return configuration;
+                    }
+                }));
+
         //csrf disable
         http
                 .csrf((auth) -> auth.disable());
