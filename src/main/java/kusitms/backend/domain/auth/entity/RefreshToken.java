af4b5f8d9903785d89e6a02bd6ce9e5ff26cac78
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-07-21 14:32:20 +0900

    [feat] RefreshToken 엔티티 생성

---
+++ src/main/java/kusitms/backend/domain/auth/entity/RefreshToken.java
@@ -0,0 +1,29 @@
+package kusitms.backend.domain.auth.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "refresh_token")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+
+    private String token;
+
+    @Builder
+    public RefreshToken(Long userId, String token) {
+        this.userId = userId;
+        this.token = token;
+    }
+
+}
