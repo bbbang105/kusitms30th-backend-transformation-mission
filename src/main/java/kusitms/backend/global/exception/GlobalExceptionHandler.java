291827934639723b08aa4ef78d2e5ffb92cc3fde
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-07-22 18:05:47 +0900

    [feat] GloabalExceptionHandler 설정

---
+++ src/main/java/kusitms/backend/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package kusitms.backend.global.exception;
+
+import kusitms.backend.global.common.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ApiResponse<Void>> handleCustomException(CustomException ex) {
+        ApiResponse<Void> response = new ApiResponse<>(ex.getStatus(), ex.getMessage(), null);
+        return new ResponseEntity<>(response, ex.getStatus());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Void>> handleException(Exception ex) {
+        ApiResponse<Void> response = new ApiResponse<>(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(), null);
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
