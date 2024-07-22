55dbbd46ab76519f46df253f90efafe8d2c09682
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-07-23 01:26:06 +0900

    [fix] User 엔티티 수정

--- src/main/java/kusitms/backend/domain/user/entity/User.java
+++ src/main/java/kusitms/backend/domain/user/entity/User.java
@@ -1,9 +1,6 @@
 package kusitms.backend.domain.user.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity(name="users")
@@ -15,10 +12,13 @@ public class User {
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 
+    @Column(nullable = false)
     private String provider;
 
+    @Column(nullable = false, unique = true)
     private String providerId;
 
     @Builder
