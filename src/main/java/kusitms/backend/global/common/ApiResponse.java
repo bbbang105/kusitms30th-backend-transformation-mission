373eb492832f912d773de42eca9394b045e1f83a
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-07-22 18:04:58 +0900

    [feat] 공통 반환 Response 설정

---
+++ src/main/java/kusitms/backend/global/common/ApiResponse.java
@@ -0,0 +1,13 @@
+package kusitms.backend.global.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public class ApiResponse<T> {
+    private final HttpStatus status;
+    private final String message;
+    private final T data;
+}
