0166c17f1182949f52cf9af03e001cf5f441984d
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-07-22 17:15:29 +0900

    [feat] User 컨트롤러, 서비스 생성

---
+++ src/main/java/kusitms/backend/domain/user/controller/UserController.java
@@ -0,0 +1,11 @@
+package kusitms.backend.domain.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+
+}
---
+++ src/main/java/kusitms/backend/domain/user/service/UserService.java
@@ -0,0 +1,12 @@
+package kusitms.backend.domain.user.service;
+
+import kusitms.backend.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+}
