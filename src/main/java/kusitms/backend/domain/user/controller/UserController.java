e1da25c217bbcd2ccc03a6c5e1928be93bf3d305
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-07-22 17:22:10 +0900

    [feat] 마이페이지 온보딩 조회 구현

--- src/main/java/kusitms/backend/domain/user/controller/UserController.java
+++ src/main/java/kusitms/backend/domain/user/controller/UserController.java
@@ -1,11 +1,21 @@
 package kusitms.backend.domain.user.controller;
 
+import kusitms.backend.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/users/{userId}")
+    public UserInfoResponse getUserInfo(@PathVariable Long userId) {
+        return UserService.getUserInfo(userId);
+    }
 
 
 }
--- src/main/java/kusitms/backend/domain/user/service/UserService.java
+++ src/main/java/kusitms/backend/domain/user/service/UserService.java
@@ -1,5 +1,8 @@
 package kusitms.backend.domain.user.service;
 
+import com.nimbusds.openid.connect.sdk.UserInfoResponse;
+import kusitms.backend.domain.onboarding.entity.UserOnBoarding;
+import kusitms.backend.domain.onboarding.repository.UserOnBoardingRepository;
 import kusitms.backend.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,5 +11,10 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserService {
 
-    private final UserRepository userRepository;
+    private final UserOnBoardingRepository userOnBoardingRepository;
+
+    public UserInfoResponse getUserInfo(Long userId) {
+        UserOnBoarding userOnBoarding=userOnBoardingRepository.findByUserId(userId);
+        return new UserInfoResponse(userOnBoarding.getNickname(),userOnBoarding.getAge(),userOnBoarding.getJob());
+    }
 }
