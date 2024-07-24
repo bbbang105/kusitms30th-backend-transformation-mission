112364c9518f991d6fa6f4990643eb85736b698c
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-07-24 10:46:11 +0900

    [feat] 스웨거 문서화 설명 추가

--- src/main/java/kusitms/backend/domain/onboarding/controller/OnboardingController.java
+++ src/main/java/kusitms/backend/domain/onboarding/controller/OnboardingController.java
@@ -1,5 +1,7 @@
 package kusitms.backend.domain.onboarding.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import kusitms.backend.domain.onboarding.dto.request.ModifyOnboardingInfoRequest;
 import kusitms.backend.domain.onboarding.dto.request.OnboardingRequest;
@@ -13,9 +15,17 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "onboarding", description = "온보딩 관련 api")
 public class OnboardingController {
     private final OnboardingService onboardingService;
 
+    @Operation(summary = "온보딩 정보 추가 입력", description = """
+            
+            userId와 추가정보인 OnboardingRequest를 이용하여 온보딩 추가정보를 입력한다.
+            
+            쿠키, 헤더 토큰 인증 필요없습니다.
+            
+            """)
     @PostMapping("/users/{userId}/onboarding")
     public ResponseEntity<ApiResponse<Void>> onboardUser(@PathVariable Long userId, @Valid @RequestBody OnboardingRequest request) {
         onboardingService.onboardUser(userId, request);
@@ -23,6 +33,13 @@ public class OnboardingController {
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
+    @Operation(summary = "온보딩 정보 조회", description = """
+            
+            userId를 통하여 해당 회원 온보딩 정보인 닉네임, 나이, 직업을 조회한다.
+            
+            헤더에 어세스토큰 인증이 필요합니다.
+            
+            """)
     @GetMapping("/users/{userId}")
     public ResponseEntity<ApiResponse<OnboardingInfoResponse>> getOnboardingInfo(@PathVariable Long userId) {
         OnboardingInfoResponse onboardingInfoResponse = onboardingService.getOnboardingInfo(userId);
@@ -30,6 +47,13 @@ public class OnboardingController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "온보딩 정보 조회", description = """
+            
+            userId와 ModifyOnboardingInfoRequest를 통하여 해당 회원 온보딩 정보인 닉네임, 나이, 직업을 수정합니다.
+            
+            헤더에 어세스토큰 인증이 필요합니다.
+            
+            """)
     @PutMapping("/users/{userId}")
     public ResponseEntity<ApiResponse<Void>> modifyOnboardingInfo(@PathVariable Long userId, @Valid @RequestBody ModifyOnboardingInfoRequest request) {
         onboardingService.modifyOnboardingInfo(userId, request);
--- src/main/java/kusitms/backend/domain/token/controller/TokenController.java
+++ src/main/java/kusitms/backend/domain/token/controller/TokenController.java
@@ -1,5 +1,7 @@
 package kusitms.backend.domain.token.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import kusitms.backend.domain.token.dto.response.TokenResponse;
 import kusitms.backend.domain.token.service.TokenService;
 import kusitms.backend.global.common.ApiResponse;
@@ -12,10 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "refresh_token", description = "토큰 관련 api")
 public class TokenController {
 
     private final TokenService tokenService;
 
+    @Operation(summary = "토큰 재발급", description = """
+            
+            리프레쉬 토큰을 이용하여 어세스토큰을 재발급합니다. (보안을 위하여 어세스토큰 재발급 시 리프레쉬토큰도 재발급)
+            
+            쿠키에 리프레쉬토큰 기입이 필요합니다.
+            
+            """)
     @PutMapping("/token/refresh")
     public ResponseEntity<ApiResponse<TokenResponse>> refreshAccessToken(@CookieValue("Refresh-Token") String refreshToken) {
         TokenResponse tokenResponse= tokenService.refreshAccessToken(refreshToken);
